53b5cbca575f7bb14a4802ffa89f0ce798616315
skkage <sakage123@icloud.com>
2022-06-26 11:45:52 +0530

    added new models to model.py

--- model.py
+++ model.py
@@ -1,1 +1,12 @@
 print("adding all ml related code here")
+
+def model1():
+    print("adding code for ml model1")
+
+
+def model2():
+    print("adding code for ml model2")
+
+
+def model3():
+    print("adding code for ml model3")
